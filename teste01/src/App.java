622c9f18f24ef4c628741a654ce35794c9db2b3b
João Marcelo Teixeira de Almeida <joaomarcello04@gmail.com>
2025-09-17 10:55:57 -0300

    Soma numeros

--- teste01/src/App.java
+++ teste01/src/App.java
@@ -1,8 +1,24 @@
-package teste;
-public class Teste {
-public static void main(String[] args) {
-int num1 = 3, num2 = 5, soma;
-soma = num1 + num2;
-System.out.println(soma);
-}
+
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int n1, n2, soma;
+
+        System.out.println("Digite o primeiro nmero: ");
+        n1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo nmero: ");
+        n2 = scanner.nextInt();
+
+        soma = n1 + n2;
+        System.out.println("A soma dos nmeros " + n1 + " e " + n2 + "  = " + soma);
+
+        scanner.close();
+
+    }
 }
